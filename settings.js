e1b4cfc9e809a4b52d09df1bf8bf1db1fd53e293
Parmita <160781867+kabbo1031@users.noreply.github.com>
2026-01-19 13:25:30 +0600

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,13 @@
+module.exports = {
+    // Target Configuration
+    TARGET_PREFIX: '000', // Looking for 0x000...
+    TARGET_SUFFIX: '',    // Leave empty to ignore
+    
+    // Search Settings
+    CASE_SENSITIVE: false,
+    THREADS: 4,
+    
+    // Output
+    SAVE_TO_FILE: true,
+    LOG_INTERVAL: 1000 // ms
+};
